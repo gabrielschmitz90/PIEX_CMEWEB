d66ebda4c8e5e2a8c35b805b37c81c529172ec8f
Gabriel Schmitz <gabrielschmitzcarlucio@gmail.com>
2026-09-22 16:17:47 -0300

    feat: Configurando o backend no Spring boot

---
+++ BACKEND/src/test/java/cmeweb_backend/CmewebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cmeweb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmewebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
